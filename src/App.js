52e26cc2cb9d1d49bbbbed8db3eb04f59b0cdcf5
NickShum <106691959+NickShum@users.noreply.github.com>
2023-01-13 20:18:54 -0500

    Bigger font

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import WEATHER_KEY from "./apikey.js";
 
-
 const api = {
   key: WEATHER_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
-function time(unix){
-  var a = new Date(unix*1000);
+function time(unix) {
+  var a = new Date(unix * 1000);
   console.log(a);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
   var hour = a.getHours();
   var min = a.getMinutes();
   var sec = a.getSeconds();
-  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  var time =
+    date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
   return time;
 }
 
@@ -33,6 +46,26 @@ function App() {
           setQuery("");
           console.log(result);
         });
+    }
+  };
+
+  const windDir = (w) => {
+    if (w > 0 && w < 90) {
+      return `N${w}°E`;
+    } else if (w > 90 && w < 180) {
+      return `S${180 - w}°E`;
+    } else if (w > 180 && w < 270) {
+      return `S${w - 180}°E`;
+    } else if (w > 270 && w < 360) {
+      return `N${360 - w}°W`;
+    } else if (w == 0) {
+      return `N`;
+    } else if (w == 90) {
+      return `E`;
+    } else if (w == 180) {
+      return `S`;
+    } else {
+      return `E`;
     }
   };
 
@@ -67,6 +100,7 @@ function App() {
 
     return `${day}, ${month} ${date} ${year}`;
   };
+
   return (
     <div
       className={
@@ -96,22 +130,19 @@ function App() {
               </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="time__box">
-              <div className="time">
-                {time((weather.dt) + (weather.timezone))}
-              </div>
-            </div>
+
             <div className="weather__box">
               <div className="weather">
-                <div className='weather__type'>
-                {weather.weather[0].main}
+                <div className="other">
+                  <div className="weather__type">{weather.weather[0].main}</div>
+                  <div className="wind">
+                    {`Wind: ${Math.round(
+                      weather.wind.speed * 3.6
+                    )} km/h ${windDir(weather.wind.deg)}`}
+                  </div>
                 </div>
-                <div className="wind">
-                  {weather.wind.speed}
-                </div>
+                <div className="temp">{Math.round(weather.main.temp)}°c</div>
               </div>
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
-              
             </div>
           </>
         ) : (
